8f0cdd24db216383159aaac5a10a2f1595b0e34e
Carlos Eduardo Ribeiro <carloseribeiro96@gmail.com>
2021-03-08 20:13:08 -0300

    Adicionado implements da inteface DaoPattern

--- src/main/java/com/grupo03/dao/EventRoomDao.java
+++ src/main/java/com/grupo03/dao/EventRoomDao.java
@@ -1,4 +1,33 @@
 package com.grupo03.dao;
 
-public class EventRoomDao {
+import com.grupo03.model.EventRoom;
+
+import java.util.List;
+import java.util.Optional;
+
+public class EventRoomDao implements DaoPattern<EventRoom> {
+    @Override
+    public Optional<EventRoom> getById(int id) {
+        return Optional.empty();
+    }
+
+    @Override
+    public List<EventRoom> getAll() {
+        return null;
+    }
+
+    @Override
+    public void save(EventRoom eventRoom) {
+
+    }
+
+    @Override
+    public void update(EventRoom eventRoom) {
+
+    }
+
+    @Override
+    public void delete(EventRoom eventRoom) {
+
+    }
 }
--- src/main/java/com/grupo03/dao/PersonDao.java
+++ src/main/java/com/grupo03/dao/PersonDao.java
@@ -1,4 +1,33 @@
 package com.grupo03.dao;
 
-public class PersonDao {
+import com.grupo03.model.Person;
+
+import java.util.List;
+import java.util.Optional;
+
+public class PersonDao implements DaoPattern<Person> {
+    @Override
+    public Optional<Person> getById(int id) {
+        return Optional.empty();
+    }
+
+    @Override
+    public List<Person> getAll() {
+        return null;
+    }
+
+    @Override
+    public void save(Person person) {
+
+    }
+
+    @Override
+    public void update(Person person) {
+
+    }
+
+    @Override
+    public void delete(Person person) {
+
+    }
 }
