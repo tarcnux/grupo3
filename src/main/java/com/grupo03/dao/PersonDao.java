b4ab007623f816ca1804ae2c1e6afabc79ad50cc
LuisNataan <luisnataan556@gmail.com>
2021-03-10 20:08:00 -0300

    Implementação dos métodos getById, getAll e save.

--- src/main/java/com/grupo03/dao/PersonDao.java
+++ src/main/java/com/grupo03/dao/PersonDao.java
@@ -1,23 +1,45 @@
 package com.grupo03.dao;
 
 import com.grupo03.model.Person;
+import com.grupo03.persistence.EntityManagerProvider;
 
 import java.util.List;
 import java.util.Optional;
 
+import javax.persistence.EntityManager;
+
 public class PersonDao implements DaoPattern<Person> {
+
+    private static EntityManager em;
+
     @Override
     public Optional<Person> getById(int id) {
-        return Optional.empty();
+        //create();
+        this.em = EntityManagerProvider.getEntityManager();
+
+        Person person = em.find(Person.class, id);
+
+        return Optional.of(person);
     }
 
     @Override
     public List<Person> getAll() {
-        return null;
+        this.em = EntityManagerProvider.getEntityManager();
+
+        List<Person> result;
+        this.em = EntityManagerProvider.getEntityManager();
+        result = this.em.createNamedQuery("SELECT p FROM person AS p", Person.class).getResultList();
+        this.em.close();
+
+        return result;
     }
 
     @Override
     public void save(Person person) {
+        this.em = EntityManagerProvider.getEntityManager();
 
+        em.getTransaction().begin();
+        em.persist(person);
+        em.getTransaction().commit();
     }
 }
