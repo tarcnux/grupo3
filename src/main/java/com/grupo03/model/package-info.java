6c36fa078cf1509cd5b66355ee04743e440c8523
Carlos Eduardo Ribeiro <carloseribeiro96@gmail.com>
2021-03-14 21:43:28 -0300

    Ajustes no texto dos comentários Javadoc;

--- src/main/java/com/grupo03/model/package-info.java
+++ src/main/java/com/grupo03/model/package-info.java
@@ -5,6 +5,7 @@
  * @author Carlos Eduardo Ribeiro (carloseduribeiro)
  * @author Giulia Marcela Mendes de Arruda (giuliaMarcela)
  * @author Guilherme Peyerl Florêncio (GuilhermePeyflo)
+ * @author Jorge Davi Navarro (jorgetbg)
  * @author Natan Luis Moreira (luisnataan)
  * @author Tarcísio Nunes (tarcnux)
  * @version 1.0
